08c46d11fc4802dc84e52a9584b36dca77da641a
Daimnon <galdrorerez@gmail.com>
2022-03-22 01:44:38 +0200

    UltraChads

--- Stoopid_X_O_By_Dush_&_Dusher/Assets/Scripts/GameManager.cs
+++ Stoopid_X_O_By_Dush_&_Dusher/Assets/Scripts/GameManager.cs
@@ -21,6 +21,11 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI[] _shapes = new TextMeshProUGUI[9];
 
+    [SerializeField]
+    private GameObject _bruhPanel;
+
+    private int _turnCounter = 0;
+
     public bool _playerOneTurn = true, _playerTwoTurn = false;
 
     private void Start()
@@ -43,48 +48,65 @@ public class GameManager : MonoBehaviour
 
     public void PositionClicked(Image tile)
     {
+        if (_turnCounter == 8)
+        {
+            print("Bruh");
+            _bruhPanel.SetActive(true);
+        }
+
         Vector3 tileTr = tile.transform.position;
 
         print($"{(Vector2)tileTr}");
         //draw the shape on the UI
 
-        for (int i = 0; i < _shapes.Length; i++)
-        {
-            if (_shapes[i].transform.parent.name == tile.name && _playerOneTurn)
-                _shapes[i].text = "X";
 
-            if (_shapes[i].transform.parent.name == tile.name && _playerTwoTurn)
-                _shapes[i].text = "O";
-        }
+        ChangeShape(tile);
 
         //update the other player about the shape
         networkManager.SendMessage($"Player Placed Shape at {tile.name}: {(Vector2)tileTr}");// your job to finish it
 
+        
         SwitchTurn();
     }
 
     private void SwitchTurn()
     {
+        _turnCounter++;
+
         if (_playerOneTurn)
-        {
-            _playerOneTurn = false;
-            _playerTwoTurn = true;
+            PlayerOneTurn();
 
-            networkManager.ListeningPort = 40001;
-            networkManager.SendingPort = 40000;
-        }
         else
-        {
-            _playerOneTurn = true;
-            _playerTwoTurn = false;
-
-            networkManager.ListeningPort = 40000;
-            networkManager.SendingPort = 40001;
-        }
+            PlayerTwoTurn();
     }
 
     private void PlayerOneTurn()
     {
+        _playerOneTurn = false;
+        _playerTwoTurn = true;
 
+        networkManager.ListeningPort = 40001;
+        networkManager.SendingPort = 40000;
+    }
+
+    private void PlayerTwoTurn()
+    {
+        _playerOneTurn = true;
+        _playerTwoTurn = false;
+
+        networkManager.ListeningPort = 40000;
+        networkManager.SendingPort = 40001;
+    }
+
+    private void ChangeShape(Image tile)
+    {
+        for (int i = 0; i < _shapes.Length; i++)
+        {
+            if (_shapes[i].transform.parent.name == tile.name && _playerOneTurn)
+                _shapes[i].text = "X";
+
+            else if (_shapes[i].transform.parent.name == tile.name && _playerTwoTurn)
+                _shapes[i].text = "O";
+        }
     }
 }
