96dd78af45027844c29e6c2738c22f0b81112712
Daimnon <galdrorerez@gmail.com>
2022-03-22 01:09:41 +0200

    Everything Works

--- Stoopid_X_O_By_Dush_&_Dusher/Assets/Scripts/GameManager.cs
+++ Stoopid_X_O_By_Dush_&_Dusher/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -17,7 +18,10 @@ public class GameManager : MonoBehaviour
      */
     #endregion
 
-    public List<Vector2> tilePosList = new List<Vector2>(9);
+    [SerializeField]
+    private TextMeshProUGUI[] _shapes = new TextMeshProUGUI[9];
+
+    public bool _playerOneTurn = true, _playerTwoTurn = false;
 
     private void Start()
     {
@@ -44,7 +48,43 @@ public class GameManager : MonoBehaviour
         print($"{(Vector2)tileTr}");
         //draw the shape on the UI
 
+        for (int i = 0; i < _shapes.Length; i++)
+        {
+            if (_shapes[i].transform.parent.name == tile.name && _playerOneTurn)
+                _shapes[i].text = "X";
+
+            if (_shapes[i].transform.parent.name == tile.name && _playerTwoTurn)
+                _shapes[i].text = "O";
+        }
+
         //update the other player about the shape
         networkManager.SendMessage($"Player Placed Shape at {tile.name}: {(Vector2)tileTr}");// your job to finish it
+
+        SwitchTurn();
+    }
+
+    private void SwitchTurn()
+    {
+        if (_playerOneTurn)
+        {
+            _playerOneTurn = false;
+            _playerTwoTurn = true;
+
+            networkManager.ListeningPort = 40001;
+            networkManager.SendingPort = 40000;
+        }
+        else
+        {
+            _playerOneTurn = true;
+            _playerTwoTurn = false;
+
+            networkManager.ListeningPort = 40000;
+            networkManager.SendingPort = 40001;
+        }
+    }
+
+    private void PlayerOneTurn()
+    {
+
     }
 }
