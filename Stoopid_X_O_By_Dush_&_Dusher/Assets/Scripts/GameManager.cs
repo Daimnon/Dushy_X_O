efe2609a8cfbfe552a98d2a2019c81319a365d6f
Daimnon <galdrorerez@gmail.com>
2022-03-21 20:59:50 +0200

    Clicking on tile & sending position

--- Stoopid_X_O_By_Dush_&_Dusher/Assets/Scripts/GameManager.cs
+++ Stoopid_X_O_By_Dush_&_Dusher/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -15,6 +16,8 @@ public class GameManager : MonoBehaviour
      [6][7][8]
      */
     #endregion
+
+    public List<Vector2> tilePosList = new List<Vector2>(9);
 
     private void Start()
     {
@@ -34,12 +37,15 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void PositionClicked(int position)
+    public void PositionClicked(Image tile)
     {
+        Vector3 tileTr = tile.transform.position;
+
+        print($"{(Vector2)tileTr}");
         //draw the shape on the UI
 
         //update the other player about the shape
-        networkManager.SendMessage("");// your job to finish it
+        networkManager.SendMessage($"{(Vector2)tileTr}");// your job to finish it
     }
 
     //for debug purpouses only
